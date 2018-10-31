dc90191b1e8dabb4d0a3e40144557487bc9abfd2
jaybaffoni <jay@baffoni5.com>
2018-10-30 23:31:26 -0400

    changed bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './eye.svg';
-import blacktext from './white-text.png';
+import blacktext from './white-text.svg';
 import './App.css';
 
 class App extends Component {
@@ -16,36 +16,25 @@ class App extends Component {
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="#">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Apps</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
         <header className="App-header">
-          <img src={logo} className="crowded-logo" alt="logo" />
+          <p>
+            <code>Welcome To</code>
+          </p>
+            <img src={blacktext} className="crowded-logo" alt="logo" />
+          
           <p>
             <code>Still Initializing</code>
           </p>
@@ -58,6 +47,20 @@ class App extends Component {
             Check Twitter
           </a>
         </header>
+
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <p>© The Crowded Studio 2018</p>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="https://twitter.com/crowded_studio" class="nav-link fa fa-twitter main-color"></a>
+      </li>
+    </ul>
+</nav>
+
       </div>
     );
   }
